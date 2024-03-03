751eaa344df70d16dbd5c46fd712e12282c58a75
Nico Ismaili <nico@ismaili.de>
2024-03-03 19:46:05 +0100

    feat: setup 11ty + twcss
    /spend 1h

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{html,md,webc,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}", // all default 11ty template types
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
