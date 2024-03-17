3368bbf59093a38593ac66a52852377cfb952ef7
Nico Ismaili <nico@ismaili.de>
2024-03-17 12:52:53 +0100

    fix: time pin alignment
    /spend 15m

--- src/components/ui/TimePin/TimePin.tsx
+++ src/components/ui/TimePin/TimePin.tsx
@@ -9,7 +9,7 @@ export const TimePin = ({ isCompleted }: Readonly<TimePinProps>) => {
   return (
     <div className="absolute h-full pt-1.5 top-0 left-0 overflow-clip">
       {isCompleted ? <Svg Icon={CircleDot} /> : <Svg Icon={Circle} />}
-      <div className="w-[0.1rem] bg-current h-full mx-auto rounded-full -mt-1.5" />
+      <div className="w-[0.13rem] bg-current h-full mx-auto rounded-full -translate-y-1.5" />
     </div>
   );
 };
