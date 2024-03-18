2a9bf18d7b49474c3466d786829d0ebb60359b98
Nico Ismaili <nico.ismaili@aoe.com>
2024-03-18 14:55:24 +0100

    feat: added gh pages depoyment workflow
    /spend 10m

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nico-i.github.io",
+  base: "/cv-gen",
   devToolbar: { enabled: false },
   integrations: [
     tailwind({
       applyBaseStyles: false,
-    })
+    }),
   ],
 });
