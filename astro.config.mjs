0100e787555b909582636aad5ff5d8e00cfc88c2
Nico Ismaili <nico@ismaili.de>
2024-03-18 21:29:35 +0100

    feat: added cname
    /spend 1m

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nico-i.github.io",
+  site: "https://cv.nico.ismaili.de",
   devToolbar: { enabled: false },
   integrations: [
     tailwind({
