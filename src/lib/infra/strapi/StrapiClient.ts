a22304c915e1aacbbaa4dba468a9a3c4297efa29
Nico Ismaili <nico.ismaili@aoe.com>
2024-03-18 15:06:35 +0100

    feat: added env vars to CI + removed unused dep
    /spend 10m

--- src/lib/infra/strapi/StrapiClient.ts
+++ src/lib/infra/strapi/StrapiClient.ts
@@ -1,6 +1,14 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "src/__generated__/gql";
 
+if (!import.meta.env.STRAPI_URL) {
+  throw new Error("STRAPI_URL is not defined");
+}
+
+if (!import.meta.env.STRAPI_API_TOKEN) {
+  throw new Error("STRAPI_API_TOKEN is not defined");
+}
+
 const gqlClient = new GraphQLClient(import.meta.env.STRAPI_URL + "/graphql", {
   headers: {
     Authorization: `bearer ${import.meta.env.STRAPI_API_TOKEN}`,
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,8 +59,7 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
     },
-  },
-  plugins: [require("tailwindcss-animate")],
+  }
 } satisfies Config;
 
 export default config;
