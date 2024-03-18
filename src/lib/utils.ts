bce5d033389df0e2f9619a4a54430ca235aabb96
Nico Ismaili <nico.ismaili@aoe.com>
2024-03-18 14:57:13 +0100

    chore: linter fixes
    /spend 2m

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,4 @@
 import { markdown } from "@astropub/md";
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
 
 export async function fetchSvgHtml(url: string): Promise<string> {
   const fetchedHtml = await fetch(url).then((res) => res.text());
